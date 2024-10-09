9fbc6960d26dfde96261ab6b9d11c5bb6c59b00e
Amy479884 <ameliafsingh@gmail.com>
2024-10-09 10:03:37 +0530

    ACP parrot

--- class/L9.3. Object oriented programming/Parrot bird Activity-3/main.py
+++ class/L9.3. Object oriented programming/Parrot bird Activity-3/main.py
@@ -9,11 +9,15 @@ class Parrot:
         # instantiate the Parrot class
 blu = Parrot("Blu", 10)
 woo = Parrot("Woo", 15)
+hoo = Parrot("Hoo" , 18)
 
 #accessing the class attributes
 print("Blu is a {}".format(blu.species))
 print("Woo is a {}".format(woo.species))
+print("Hoo is a {}".format(hoo.species))
+
 
 #accessing the instant attributes
 print("{} is {} years old".format( blu.name, blu.age))
 print("{} is {} years old".format( woo.name, woo.age))
+print("{} is {} years old".format( hoo.name, hoo.age))
