bbed6c796e45c415c1bc8ec1bd228f5cf9cfe189
Amy479884 <ameliafsingh@gmail.com>
2024-10-18 11:03:30 +0530

    File handling operation- 1

---
+++ mine/Practicals/main.py
@@ -0,0 +1,23 @@
+# Function to check whether the argument being passed is even or odd
+
+def CALCULATE(num):
+
+ if num % 2 == 0:
+
+  print("Even")
+
+ else:
+
+     print("Odd")
+
+# Function to return the sum of two numbers being passed as arguments
+
+def CALCULATE(num1, num2):
+
+  return num1 + num2
+
+# Function to display the message "YOU ARE DOING GREAT"
+
+def CALCULATE():
+
+ print("YOU ARE DOING GREAT")
