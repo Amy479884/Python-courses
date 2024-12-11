0ca4fa0f9bbf1c04af9c31f12430dfc41a952157
Amy479884 <ameliafsingh@gmail.com>
2024-12-11 11:06:43 +0530

    Tkinter activity 1

--- class/L 11.1 File  hhandling/File handling operations- part 2/File handling operations 1/main.py
+++ class/L 11.1 File  hhandling/File handling operations- part 2/File handling operations 1/main.py
@@ -15,4 +15,32 @@ file.close()
 print("\n")
 print("\n")
 
+# Third project duplicate lines
+
+outputFile = open('UpdatedFile.txt', "w")
+
+#reading the input file
+inputFile = open('Repeated.txt', "r")
+
+# holds lines already seen
+lines_seen_so_far = set()
+print("Eliminated duplicate lines, to see the output open the updated file....")
+#iterating each line in the file
+for line in inputFile:
+
+    # checking if the line is unique
+   if line not in lines_seen_so_far:
+       
+       #write unique lines in output file
+       outputFile.write(line)
+
+       #adds unique lines 
+       lines_seen_so_far.add(line)
+
+# closing the file
+inputFile.close()
+outputFile.close()
+
+
+
 
