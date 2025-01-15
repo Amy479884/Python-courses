2547866e46dd5715c8dc08a6c07a1e1310206043
Amy479884 <ameliafsingh@gmail.com>
2025-01-15 14:14:58 +0530

    creating  a tabel

--- class/L11.9 Database/Create database/index.js
+++ class/L11.9 Database/Create database/index.js
@@ -4,12 +4,23 @@ var con = mysql.createConnection({
     host: "remotemysql.com", // Give your host name
     user: "Amelia", // Give your username
     password: "Honeysingh1!", // Give your password
+    database: "sql6437074"
+});
+connection.connect((err) =>{
+if(err) throw err
+console.log("connected");
+var sql = "CREATE TABLE Student(Student_ID INT, Student_FirstName VARCHAR(255),Student_LastName VARCHAR(255), sTUDENT_City VARCHAR(255))";
+connection.query(sql,function(err,result){
+    if(err) throw err;
+    console.log("Tabel created in DB");
+});
 });
 
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected");
-});
+
+
+
+
+
 
 
 
