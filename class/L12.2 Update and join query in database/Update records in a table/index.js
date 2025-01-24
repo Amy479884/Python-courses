dbe643a975c34554fb23a64ee74a57d3ba9ff3f5
Amy479884 <ameliafsingh@gmail.com>
2025-01-24 12:42:29 +0530

    update records in table

---
+++ class/L12.2 Update and join query in database/Update records in a table/index.js
@@ -0,0 +1,18 @@
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "sql12.freesqldatabase.com",
+    user: "sql12758131",
+    password: "rEhwYw1qJC",
+    database: "sql12758131"
+});
+connection.connector((err) => {
+    if (err) throw err
+    console.log("connected");
+
+var  sql = "UPDATE Students SET Student_City='Mumbai' WHERE Student_ID = 102";
+connection.query(sql,function(err,result){
+    if(err) throw err;
+    console.log("Data Updated in Table");
+});
+});
