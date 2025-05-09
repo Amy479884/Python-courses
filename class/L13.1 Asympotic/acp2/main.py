0a647927e76b145a1c4c185896bb4ea59f1825f1
Amy479884 <ameliafsingh@gmail.com>
2025-05-09 11:05:11 +0530

    best condition

---
+++ class/L13.1 Asympotic/acp2/main.py
@@ -0,0 +1,8 @@
+def printnumber(n):
+    iteration = 0
+    print("the total number entered by computer is",n) 
+    iteration+=1 
+    print("total iteration done by computer is",iteration,"\n")
+printnumber(10)
+printnumber(200)
+print("\n with any'n' the time taken by the computer will not change")
